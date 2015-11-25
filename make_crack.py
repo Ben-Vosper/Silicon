403c951c53af68d6f740402b1092531e40425b52
Ben-Vosper <ben-vosper@hotmail.com>
2015-11-25 14:29:09 +0000

    Crack slab made and exported.

--- make_crack.py
+++ make_crack.py
@@ -44,6 +44,10 @@ si_bulk = bulk('Si', 'diamond', a=5.431, cubic=True)
 mm_pot = Potential("IP SW")
 si_bulk.set_calculator(mm_pot)
 
+c = mm_pot.get_elastic_constants(si_bulk)
+E = youngs_modulus(c, cleavage_plane)
+nu = poisson_ratio(c, cleavage_plane, crack_direction)
+
 unit_slab = Diamond(directions=[crack_direction,
                                 cleavage_plane,
                                 crack_front],
@@ -57,8 +61,18 @@ unit_slab.positions[:, 1] += (unit_slab.positions[1, 1] -
                               unit_slab.positions[0, 1]) / 2.0
 unit_slab.set_scaled_positions(unit_slab.get_scaled_positions())
 
+
+
 surface = unit_slab.copy()
 surface.center(vacuum, axis=1)
+
+surface.set_calculator(mm_pot)
+E_surf = surface.get_potential_energy()
+E_per_atom_bulk = si_bulk.get_potential_energy() / len(si_bulk)
+area = surface.get_volume() / surface.cell[1, 1]
+gamma = ((E_surf - E_per_atom_bulk * len(surface)) /
+         (2.0 * area))
+
 
 nx = int(width / unit_slab.cell[0, 0])
 ny = int(height / unit_slab.cell[1, 1])
@@ -85,75 +99,64 @@ orig_width = (crack_slab.positions[:, 0].max() -
 orig_height = (crack_slab.positions[:, 1].max() -
                crack_slab.positions[:, 1].min())
 
-atomeye.view(crack_slab)
-while 1 ==1:
-    pass
 
-# print(('Made slab with %d atoms, original width and height: %.1f x %.1f A^2' %
-#        (len(crack_slab), orig_width, orig_height)))
-#
-# top = crack_slab.positions[:, 1].max()
-# bottom = crack_slab.positions[:, 1].min()
-# left = crack_slab.positions[:, 0].min()
-# right = crack_slab.positions[:, 0].max()
-#
-# # fix atoms in the top and bottom rows
-# fixed_mask = ((abs(crack_slab.positions[:, 1] - top) < 1.0) |
-#               (abs(crack_slab.positions[:, 1] - bottom) < 1.0))
-# const = FixAtoms(mask=fixed_mask)
-# crack_slab.set_constraint(const)
-# print('Fixed %d atoms\n' % fixed_mask.sum())
-#
-#
-# # ****** Apply initial strain ramp *****
-#
-# strain = G_to_strain(initial_G, E, nu, orig_height)
-#
-# crack_slab.positions[:, 1] += thin_strip_displacement_y(
-#                                  crack_slab.positions[:, 0],
-#                                  crack_slab.positions[:, 1],
-#                                  strain,
-#                                  left + crack_seed_length,
-#                                  left + crack_seed_length + strain_ramp_length)
-#
-# print('Applied initial load: strain=%.4f, G=%.2f J/m^2' %
-#       (strain, initial_G / (units.J / units.m**2)))
-#
-#
-# # ***** Relaxation of crack slab  *****
-#
-# # optionally, relax the slab, keeping top and bottom rows fixed
-# print('Relaxing slab...')
-# crack_slab.set_calculator(mm_pot)
-# minim = Minim(crack_slab, relax_positions=True, relax_cell=False)
-# minim.run(fmax=relax_fmax)
-#
-# # Find initial position of crack tip
-# crack_pos = find_crack_tip_stress_field(crack_slab, calc=mm_pot)
-# print 'Found crack tip at position %s' % crack_pos
-#
-# # Save all calculated materials properties inside the Atoms object
-# crack_slab.info['nneightol'] = 1.3 # nearest neighbour tolerance
-# crack_slab.info['LatticeConstant'] = a0
-# crack_slab.info['C11'] = c[0, 0]
-# crack_slab.info['C12'] = c[0, 1]
-# crack_slab.info['C44'] = c[3, 3]
-# crack_slab.info['YoungsModulus'] = E
-# crack_slab.info['PoissonRatio_yx'] = nu
-# crack_slab.info['SurfaceEnergy'] = gamma
-# crack_slab.info['OrigWidth'] = orig_width
-# crack_slab.info['OrigHeight'] = orig_height
-# crack_slab.info['CrackDirection'] = crack_direction
-# crack_slab.info['CleavagePlane'] = cleavage_plane
-# crack_slab.info['CrackFront'] = crack_front
-# crack_slab.info['strain'] = strain
-# crack_slab.info['G'] = initial_G
-# crack_slab.info['CrackPos'] = crack_pos
-# crack_slab.info['is_cracked'] = False
-#
-#
-# # ******** Save output file **********
-#
-# # save results in extended XYZ format, including extra properties and info
-# print('Writing crack slab to file %s' % output_file)
-# write(output_file, crack_slab)
+
+top = crack_slab.positions[:, 1].max()
+bottom = crack_slab.positions[:, 1].min()
+left = crack_slab.positions[:, 0].min()
+right = crack_slab.positions[:, 0].max()
+
+# fix atoms in the top and bottom rows
+fixed_mask = ((abs(crack_slab.positions[:, 1] - top) < 1.0) |
+              (abs(crack_slab.positions[:, 1] - bottom) < 1.0))
+const = FixAtoms(mask=fixed_mask)
+crack_slab.set_constraint(const)
+
+# ****** Apply initial strain ramp *****
+
+strain = G_to_strain(initial_G, E, nu, orig_height)
+
+crack_slab.positions[:, 1] += thin_strip_displacement_y(
+                                 crack_slab.positions[:, 0],
+                                 crack_slab.positions[:, 1],
+                                 strain,
+                                 left + crack_seed_length,
+                                 left + crack_seed_length + strain_ramp_length)
+
+print('Applied initial load: strain=%.4f, G=%.2f J/m^2' %
+      (strain, initial_G / (units.J / units.m**2)))
+
+
+crack_slab.set_calculator(mm_pot)
+minim = Minim(crack_slab, relax_positions=True, relax_cell=False)
+minim.run(fmax=relax_fmax)
+
+# Find initial position of crack tip
+crack_pos = find_crack_tip_stress_field(crack_slab, calc=mm_pot)
+print 'Found crack tip at position %s' % crack_pos
+
+# Save all calculated materials properties inside the Atoms object
+crack_slab.info['nneightol'] = 1.3 # nearest neighbour tolerance
+crack_slab.info['LatticeConstant'] = 5.431
+crack_slab.info['C11'] = c[0, 0]
+crack_slab.info['C12'] = c[0, 1]
+crack_slab.info['C44'] = c[3, 3]
+crack_slab.info['YoungsModulus'] = E
+crack_slab.info['PoissonRatio_yx'] = nu
+crack_slab.info['SurfaceEnergy'] = gamma
+crack_slab.info['OrigWidth'] = orig_width
+crack_slab.info['OrigHeight'] = orig_height
+crack_slab.info['CrackDirection'] = crack_direction
+crack_slab.info['CleavagePlane'] = cleavage_plane
+crack_slab.info['CrackFront'] = crack_front
+crack_slab.info['strain'] = strain
+crack_slab.info['G'] = initial_G
+crack_slab.info['CrackPos'] = crack_pos
+crack_slab.info['is_cracked'] = False
+
+
+# ******** Save output file **********
+
+# save results in extended XYZ format, including extra properties and info
+print('Writing crack slab to file %s' % output_file)
+write(output_file, crack_slab)
